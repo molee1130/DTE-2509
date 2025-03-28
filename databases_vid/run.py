fcfffb6b911927fac5230ddc4099a8e5aff832ca
molee1130 <molee1130@uit.no>
2025-03-28 04:07:17 +0100

    update gitignore

--- databases_vid/run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-# can not directly import (flask, sqlalchemy etc?). Will cause circular import statments
-# therefor we call create_app, who then use the imports  
-flask_app = create_app()
-
-if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", debug=True)
